37ace2e1c0ffec1517af8f197db0a9db13fb23ec
Nersik Militosyan <militosyan13@gmail.com>
2024-07-24 00:18:41 +0400

    function for createPost is volid

--- controller/taskController.js
+++ controller/taskController.js
@@ -2,6 +2,56 @@ import { v4 as uuid } from 'uuid'
 import validate from '../utils/validate.js'
 let posts = []
 
-async function createPost(req, res) {}
+async function createPost(req, res) {
+	try {
+		const errors = validate.createTask(req)
+
+		if (errors.haveErrors) {
+			res.status(422).json({
+				errors: errors.fields,
+				message: 'Validation error',
+			})
+			return
+		}
+		const { title, description, taskDate } = req.body
+
+		const allowToCreate = checkTaskDate(taskDate)
+
+		if (!allowToCreate) {
+			res.status(422).json({
+				message: 'You can create a task for same day max 3 times.',
+			})
+			return
+		}
+
+		const newData = {
+			id: uuid(),
+			title,
+			description,
+			taskDate,
+			completed: false,
+		}
+
+		posts.push(newData)
+
+		res
+			.status(200)
+			.json({ message: 'createPost successful', data: posts, status: 200 })
+	} catch (e) {
+		res.status(404).json({ message: e.message, status: 404 })
+	}
+}
+
+function checkTaskDate(taskDate) {
+	let count = 0
+
+	posts.forEach(task => {
+		if (task.taskDate === taskDate) {
+			count++
+		}
+	})
+
+	return count < 3
+}
 
 export default { createPost }
--- utils/validate.js
+++ utils/validate.js
@@ -1,7 +1,64 @@
 import Joi from 'joi'
 
+const taskSchema = Joi.object({
+	title: Joi.string()
+		.min(3)
+		.max(100)
+		.required()
+		.pattern(/^([a-zA-Z0-9]( )?)+$/)
+		.messages({
+			'string.base': 'Title field must be a string',
+			'string.empty': 'Title field is required',
+			'string.min': 'Title must be at least {#limit} characters long',
+			'string.max': 'Title cannot exceed {#limit} characters',
+			'any.required': 'Title field is required (min 3, max 100 characters)',
+			'string.pattern.base':
+				'Invalid description value: allow only text, number, and space (max space count between word is one space)',
+		}),
+	description: Joi.string()
+		.min(3)
+		.max(5000)
+		.required()
+		.pattern(/^([a-zA-Z0-9]( )?)+$/)
+		.messages({
+			'string.base': 'Description field must be a string',
+			'string.empty': 'Description field is required',
+			'string.min': 'Description must be at least {#limit} characters long',
+			'string.max': 'Description cannot exceed {#limit} characters',
+			'any.required': 'Description field is required',
+			'string.pattern.base':
+				'Invalid description value: allow only text, number, and space (max space count between word is one space)',
+		}),
+	taskDate: Joi.date().iso().greater('now').required().messages({
+		'date.base': 'Task date must be a valid date',
+		'date.isoDate': 'Invalid date format: YYYY-mm-dd',
+		'date.greater': 'Please provide a future date',
+		'any.required': 'Task date is required',
+	}),
+})
+
 function createTask(req) {
-	//
+	const { error, value } = taskSchema.validate(req.body, {
+		abortEarly: false,
+	})
+
+	if (error) {
+		const fields = {}
+		error.details.forEach(detail => {
+			console.log(detail)
+			fields[detail.path[0]] = detail.message
+		})
+
+		return {
+			fields,
+			haveErrors: true,
+		}
+	}
+
+	return {
+		fields: {},
+		haveErrors: false,
+	}
 }
 
 export default { createTask }
